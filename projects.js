b2368a47b6914e607d6217d7fe346b16532f118b
sang.park4 <sangpk@outlook.com>
2021-01-05 19:27:23 -0700

    Fifth Commit

---
+++ projects.js
@@ -0,0 +1,37 @@
+let cards = []
+
+const invokeProto = (projIdx) => {
+    cards[projIdx].display()
+}
+
+const Card = function(elem, plus, minus) {
+    this.elem = elem;
+    this.plus = plus;
+    this.minus = minus;
+    this.opened = false;
+}
+
+Card.prototype.display = function(){
+    if (this.opened) {
+        this.elem.style.maxHeight = '0px';
+        this.plus.style.display = 'block';
+        this.minus.style.display = 'none';
+        this.opened = false
+    } else {
+        this.elem.style.maxHeight = '1100px';
+        this.plus.style.display = 'none';
+        this.minus.style.display = 'block';
+        this.opened = true;
+    }
+}
+
+function init() {
+    const elemArr = document.querySelectorAll('.accordian-item');
+    for(let i = 0; i < elemArr.length; ++i) {
+        cards.push(new Card(document.querySelector(`#project${i + 1} .projectdescription`),
+                            document.querySelector(`#project${i + 1} .accordian-link .fa-plus`),
+                            document.querySelector(`#project${i + 1} .accordian-link .fa-minus`)));
+    }
+}
+
+init();
